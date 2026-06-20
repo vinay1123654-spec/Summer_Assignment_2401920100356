d12057bb0b48e5f90655e51a48e8fbc4c0a3a4e5
vinay1123654-spec <vinay1123654@gmail.com>
2026-06-20 08:30:00 +0530

    Implement queue using two stacks

---
+++ Week_3/Day_5/Implement_Queue_Using_Stack.cpp
@@ -0,0 +1,33 @@
+class MyQueue {
+public:
+    stack<int> s1, s2;
+
+    MyQueue() {
+        
+    }
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        peek();
+        int val = s2.top();
+        s2.pop();
+        return val;
+    }
+
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
