3e824b17070fb02636f63eb8b056aebd9ca79f18
vinay1123654-spec <vinay1123654@gmail.com>
2026-06-27 13:03:59 +0530

    Implement diameter calculation for binary tree

---
+++ Week_4/Day_4/Diameter_Of_Binary_Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int diameter = 0;
+
+    int height(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int left = height(root->left);
+        int right = height(root->right);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return diameter;
+    }
+};
